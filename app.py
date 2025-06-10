1d8d8fe8da6a3ce14a805ec741ab2d6dd3abc13c
1-1ii <167253057+1-1ii@users.noreply.github.com>
2025-06-11 00:03:58 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+from analysis_and_model import analysis_and_model_page
+from presentation import presentation_page
+
+pages = {
+    "🔬Анализ и модель": analysis_and_model_page,
+    "📺Презентация": presentation_page
+}
+
+selected_page = st.sidebar.selectbox("Список страниц", list(pages.keys()))
+pages[selected_page]()
